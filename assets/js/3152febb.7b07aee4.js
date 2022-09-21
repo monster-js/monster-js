67acf4f9b130bbe8290aabb9476da0a4d5496739
Darius Bualan <mfpjayb@gmail.com>
2022-09-22 01:31:07 +0800

    Deploy website - based on 7c99bd442fa2db946de96f5723897673287c49d2

---
+++ assets/js/3152febb.7b07aee4.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_monster_js_docs=self.webpackChunk_monster_js_docs||[]).push([[225],{3492:t=>{t.exports=JSON.parse('{"title":"Getting Started","slug":"/category/getting-started","permalink":"/docs/category/getting-started","navigation":{"next":{"title":"What is MonsterJS","permalink":"/docs/getting-started/what-is-monster-js"}}}')}}]);
---
+++ src/custom.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+  /**
+   * Google analytics
+   */
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-229775239-1');
+}
