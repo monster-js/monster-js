042000e2d333850ac2e7487e53149c313719a6f2
Darius Bualan <darius.bualan@synlab.com>
2022-11-21 12:10:05 +0800

    fixed attrBoolean bug that returns true if attribute not present in the element

--- projects/core/src/observed-attributes/attr-boolean.ts
+++ projects/core/src/observed-attributes/attr-boolean.ts
@@ -2,7 +2,7 @@ import { ComponentInstance } from "../component/interfaces/component-instance.in
 import { attribute } from "./attribute";
 
 export const attrBoolean = (context: ComponentInstance, attributeName: string, changeWatcher?: (newValue: any, oldValue: any) => void) => {
-    const falsy = ['null', 'undefined', '', '0', 'false'];
+    const falsy = [null, undefined, 'null', 'undefined', '', '0', 'false'];
     const value = attribute(
         context,
         attributeName,
