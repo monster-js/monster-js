f9684277e4342e1c2124663fbf09845cbec17c84
Darius Bualan <darius.bualan@synlab.com>
2022-12-08 20:21:57 +0800

    changed structural directives properties to optional

--- projects/core/src/component/interfaces/structural-directives.interface.ts
+++ projects/core/src/component/interfaces/structural-directives.interface.ts
@@ -1,7 +1,7 @@
 export interface StructuralDirectives {
-    ['v:if']: any;
-    ['v:for']: any;
-    ['v:for-item']: any;
-    ['v:for-index']: any;
-    ['v:for-update']: any;
+    ['v:if']?: any;
+    ['v:for']?: any;
+    ['v:for-item']?: any;
+    ['v:for-index']?: any;
+    ['v:for-update']?: any;
 }
