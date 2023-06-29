0984e869f19f493abbaffa4e5d1ee6a11fb64b4a
Darius Bualan <darius.bualan@synlab.com>
2023-06-29 21:27:14 +0800

    updated store create store, create action, and added create selector

--- store/src/create-selector.ts
+++ store/src/create-selector.ts
@@ -2,10 +2,10 @@ import { FunctionComponent } from "@monster-js/core";
 import { StoreFn } from "./create-store";
 
 export function createSelector<T, K extends keyof T>(store: StoreFn<T>, key: K) {
-    return function<C = any>(callback: (state: T[K]) => C) {
+    return function<C, A = any>(callback: (state: T[K], params?: A) => C) {
         return function(context: FunctionComponent) {
             const [getter] = store(context, key);
-            return () => callback(getter());
+            return (params?: A) => callback(getter(), params);
         };
     }
 }
