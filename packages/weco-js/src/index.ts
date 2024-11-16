14b347436a984f22403b69b72e5a1ad939671ef9
darius <mfpjayb@gmail.com>
2024-11-16 20:33:15 +0800

    fixed tree shaking

--- packages/weco-js/src/index.ts
+++ packages/weco-js/src/index.ts
@@ -1,16 +1,16 @@
 // dependency injection
-export * from './di/create-di-container';
-export * from './di/default-di-container';
+export { createDIContainer } from './di/create-di-container';
+export { inject, overrideProvider } from './di/default-di-container';
 
 // template engine
-export * from './template-engine/define-component';
-export * from './template-engine/create-element';
-export * from './template-engine/create-component';
-export * from './template-engine/add-event-listener';
-export * from './template-engine/append-children';
-export * from './template-engine/create-text-node';
-export * from './template-engine/bind-text-node';
-export * from './template-engine/bind-attributes';
-export * from './template-engine/if-condition';
-export * from './template-engine/for-loop';
-export * from './template-engine/apply-props';
+export { defineComponent } from './template-engine/define-component';
+export { createElement } from './template-engine/create-element';
+export { createComponent } from './template-engine/create-component';
+export { addEventListener } from './template-engine/add-event-listener';
+export { appendChildren } from './template-engine/append-children';
+export { createTextNode } from './template-engine/create-text-node';
+export { bindTextNode } from './template-engine/bind-text-node';
+export { bindAttributes } from './template-engine/bind-attributes';
+export { ifCondition } from './template-engine/if-condition';
+export { forLoop } from './template-engine/for-loop';
+export { applyProps } from './template-engine/apply-props';
